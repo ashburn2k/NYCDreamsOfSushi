28857257c6b55dffc3201759a89d0b761cb6f72f
kimb90 <klin@dramafever.com>
2015-07-07 11:12:06 -0400

    document ready

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-$.getJSON('yelp_data.json').then(function (yelp_data) {
+$(document).ready(function() {
 
-  var response = yelp_data;
+  $.getJSON('yelp_data.json').then(function (yelp_data) {
 
-  console.log(response);
+    var response = yelp_data;
 
-  var length = response.businesses.length; 
+    console.log(response);
 
-  for (var i = 0; i < length; i++) {
+    var length = response.businesses.length; 
 
-    var restaurant = $('<div id="' + i + '" class="collection">'
-          + '<a href="' + response.businesses[i].url + '">'
-            + '<img width="300px" height="300px" src="' + response.businesses[i].image_url + '">'
-          + '</a>'
-          + '<h4>' + response.businesses[i].name + '</h4>' 
-          + '<img class="rating_img" src="' + response.businesses[i].rating_img_url + '">'
-          + '<p>' + response.businesses[i].location.display_address[0] + '</p>'
-          + '<p>' + response.businesses[i].location.display_address[1] + '</p>'
-          + '<p>' + response.businesses[i].location.display_address[2] + '</p>'
-          + '<p>' + response.businesses[i].display_phone + '</p>'
-          + '<span class="change-icon">'
-            + '<i class="fa fa-heart-o fa-lg"></i>'
-            + '<i class="fa fa-heart fa-lg"></i>'
-          + '</span>'
-      + '</div>');
+    for (var i = 0; i < length; i++) {
 
-    $(restaurant).on('click', '.change-icon', function(){
-      console.log($(this).parent()[0].id);
-    });
+      var restaurant = $('<div id="' + i + '" class="collection">'
+            + '<a href="' + response.businesses[i].url + '">'
+              + '<img width="300px" height="300px" src="' + response.businesses[i].image_url + '">'
+            + '</a>'
+            + '<h4>' + response.businesses[i].name + '</h4>' 
+            + '<img class="rating_img" src="' + response.businesses[i].rating_img_url + '">'
+            + '<p>' + response.businesses[i].location.display_address[0] + '</p>'
+            + '<p>' + response.businesses[i].location.display_address[1] + '</p>'
+            + '<p>' + response.businesses[i].location.display_address[2] + '</p>'
+            + '<p>' + response.businesses[i].display_phone + '</p>'
+            + '<span class="change-icon">'
+              + '<i class="fa fa-heart-o fa-lg"></i>'
+              + '<i class="fa fa-heart fa-lg"></i>'
+            + '</span>'
+        + '</div>');
 
-    $('.restaurantsContainer').append(restaurant);
+      $(restaurant).on('click', '.change-icon', function(){
+        console.log($(this).parent()[0].id);
+      });
 
-  };
+      $('.restaurantsContainer').append(restaurant);
+
+    };
+
+  });
+
+  // Favorites
+
+  var FavoritesArr = [];
+
 
 });
-
-// Favorites
-
-var FavoritesArr = [];
