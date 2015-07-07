a6b4427701ef8874a40e6e0f3f5fc520a5ffa9f9
kimb90 <klin@dramafever.com>
2015-07-07 17:35:47 -0400

    favorites

--- script.js
+++ script.js
@@ -41,7 +41,26 @@ $(document).ready(function() {
     var favoritesArray = ["3", "2", "1", "0"];
 
     $.each(favoritesArray, function(index, value) {
-      console.log(response.businesses[value].name);
+      // console.log(response.businesses[value].name);
+
+      var favorite = $('<div id="' + i + '" class="collection">'
+            + '<a href="' + response.businesses[value].url + '">'
+              + '<img width="300px" height="300px" src="' + response.businesses[value].image_url + '">'
+            + '</a>'
+            + '<h4>' + response.businesses[value].name + '</h4>' 
+            + '<img class="rating_img" src="' + response.businesses[value].rating_img_url + '">'
+            + '<p>' + response.businesses[value].location.display_address[0] + '</p>'
+            + '<p>' + response.businesses[value].location.display_address[1] + '</p>'
+            + '<p>' + response.businesses[value].location.display_address[2] + '</p>'
+            + '<p>' + response.businesses[value].display_phone + '</p>'
+            + '<span class="change-icon">'
+              + '<i class="fa fa-heart-o fa-lg"></i>'
+              + '<i class="fa fa-heart fa-lg"></i>'
+            + '</span>'
+        + '</div>');
+
+      $('.favoritesContainer').append(favorite);
+
     });
 
   });
